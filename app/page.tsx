a81aa910ceb6902034d88358c6f0061aab9b1678
Harel <harelz987@gmail.com>
2024-07-27 06:05:22 +0300

    Added Firebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
-'use client';
 import React, { useEffect } from 'react';
 
 export default function Home() {
-  useEffect(() => {
-    console.log(process.env.FIREBASE_API_KEY);
-  }, []);
-
-
   return (
     <div className="h-body flex w-full flex-col items-center justify-center">
       <h1 className="text-center text-3xl">Under development.</h1>
