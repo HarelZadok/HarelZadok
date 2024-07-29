46acf806a9aec501543849a21e7da5df6352202c
Harel <harelz987@gmail.com>
2024-07-30 02:38:15 +0300

    Files implemented

--- app/files/FileList.tsx
+++ app/files/FileList.tsx
@@ -1,17 +1,36 @@
-import { getPublicFiles } from '@/lib/firebase/firebaseActions';
-import React from 'react';
-import Link from 'next/link';
+'use client';
 
-export default async function FileList() {
-  const files = await getPublicFiles();
+import React from 'react';
+import clsx from 'clsx';
+import { useTheme } from 'next-themes';
+
+export default function FileList({ files }: { files: { name: string; url: string }[] }) {
+  const { theme } = useTheme();
+
+  const downloadFileByLink = (fileName: string, url: string) => {
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', fileName);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   return (
-    <div>
-      {files.map((file) => (
-        <Link href={file.url} key={file.name}>
-          {file.name}
-        </Link>
-      ))}
+    <div className="h-body flex w-full items-center justify-center text-center">
+      <div
+        className={clsx('w-full max-w-5xl rounded-lg p-8 shadow-lg', {
+          ['bg-gray-100']: theme === 'light',
+          ['bg-[rgb(50,50,50)]']: theme === 'dark',
+        })}
+      >
+        <h1 className="text-4xl font-bold">Files</h1>
+        {files.map((file) => (
+          <button onClick={() => downloadFileByLink(file.name, file.url)} key={file.name}>
+            {file.name}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
--- app/files/page.tsx
+++ app/files/page.tsx
@@ -1,24 +1,13 @@
-'use client';
-
+import { getPublicFiles } from '@/lib/firebase/firebaseActions';
 import React from 'react';
-import clsx from 'clsx';
-import { useTheme } from 'next-themes';
 import FileList from './FileList';
 
-export default function Profile() {
-  const { theme } = useTheme();
+export default async function Files() {
+  const files = await getPublicFiles();
 
   return (
-    <div className="h-body flex w-full items-center justify-center text-center">
-      <div
-        className={clsx('w-full max-w-5xl rounded-lg p-8 shadow-lg', {
-          ['bg-gray-100']: theme === 'light',
-          ['bg-[rgb(50,50,50)]']: theme === 'dark',
-        })}
-      >
-        <h1 className="text-4xl font-bold">Files</h1>
-        <FileList />
-      </div>
+    <div>
+      <FileList files={files} />
     </div>
   );
 }
