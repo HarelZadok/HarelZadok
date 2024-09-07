a9677c2c3d90614cdc98f318baadca123cc1980f
Harel <harelz987@gmail.com>
2024-09-08 01:08:53 +0300

    Login screen updates

--- app/(auth)/AuthFormButton.tsx
+++ app/(auth)/AuthFormButton.tsx
@@ -9,15 +9,23 @@ import { useTheme } from 'next-themes';
 export default function AuthFormButton({ title, href }: { title: string; href: string }) {
   const pathname = usePathname();
   const { theme } = useTheme();
+  const [hovered, setHovered] = React.useState(false);
 
   return (
     <Link
       href={href}
+      onMouseEnter={() => {
+        setHovered(true);
+      }}
+      onMouseLeave={() => {
+        setHovered(false);
+      }}
       className={clsx(
-        'flex-1 rounded pb-3 py-2 text-base font-bold underline-offset-[15px] transition-opacity duration-700 ease-in-out',
+        'flex-1 rounded py-2 pb-3 text-base font-bold underline-offset-[15px] transition-opacity duration-500 ease-in-out',
         {
           'border-b-2 opacity-100': pathname === href,
-          'opacity-40': pathname !== href,
+          'opacity-40': pathname !== href && !hovered,
+          'opacity-75': pathname !== href && hovered,
           'border-gray-500': theme === 'light',
           'border-white': theme === 'dark',
         },
